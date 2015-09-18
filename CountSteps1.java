0505917716914918d8d7f7fc3986abd80f9c4956
rcrazy88 <rcrazy88.007@gmail.com>
2015-09-18 18:24:29 +0530

    counting minimum no. of steps to get desired array

---
+++ CountSteps1.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+
+class CountSteps1
+	{
+		public static void main(String... s)
+		{
+			int sum=55;
+			int count=0;
+			int zero=1;
+
+			CountSteps1 cs= new CountSteps1();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter the number of elements you will enter");
+			int m = sc.nextInt();
+			
+			System.out.println("enter"+ m + " values: ");
+			int b[] = new int[m];
+			for(int i = 0; i < m; i++){
+			    b[i] = sc.nextInt();
+			}
+			
+		while(zero==1)
+			{
+			zero = cs.checkingForAllZero(b);
+			
+			if(cs.checkingSum(b)==1)
+				{
+				count=	cs.divisionByTwo(b,count);  //gadbad!!!! call it only when all elements are even
+				} 
+			else 
+			{
+			count=	cs.checkingForOdd(b,count);   
+			}
+
+			 
+			}
+			
+System.out.println(count);
+
+	}
+	
+		int checkingForAllZero(int a[])   //checking for all zero
+			{
+			int sum = 0;
+			int i;
+				for(i=0;i<a.length;i++)
+				{
+					
+					sum = sum+ a[i];
+				}
+				
+				if(sum==0)
+					 return 0;
+				else
+					return 1;
+			}
+	
+	
+		int checkingForOdd(int a[], int count)   //checking for any odd element in an array
+			{
+			    
+				for(int i=0;i<a.length;i++)
+				{
+					System.out.print(a[i]+ " ");
+					if(a[i]%2==1)
+					{
+					int temp = a[i];
+					temp = temp-1;
+					a[i]=temp;
+					count++ ;
+					
+					}
+				}
+				System.out.println(" ");
+			return count;
+			}
+			
+		int checkingSum(int a[])   //checking for sum even or odd..... wrong code!!!  (sum!=0 && all elements are even)
+			{ 
+				int flag=0;
+				int sum=0;
+				int value=555;
+					for(int i=0;i<a.length;i++)
+					{
+					  sum = sum+ a[i];
+					}
+
+				for(int i=0;i<a.length;i++)
+				{
+					if (a[i]%2==1)
+						{
+							flag=1;
+							break;
+						}
+				}
+				
+			if((flag==0)&&(sum!=0))
+			{	value=1;  }
+			
+			return value;
+			}
+			
+			
+			
+	
+		int divisionByTwo(int a[], int count)     // dividing all elements by 2
+		{
+			for(int i=0;i<a.length;i++)
+			{
+				System.out.print(a[i]+ " ");
+				a[i]= a[i]/2;
+			}
+			System.out.println(" ");
+			count= count+1;
+		return count;
+		}
+	
+	}
